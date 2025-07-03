38d255a2c64b44ec368867474b3b5d14987d4de7
Jawad <jawad.hassan.office@gmail.com>
2025-07-03 05:11:50 +0300

    add basic sturcher and save [currentLevel, levelLiveLocation, totalLivesLocation]

---
+++ script.js
@@ -0,0 +1,4 @@
+let currentLevel = document.getElementById("currentLevel").innerHTML = "is work"
+let levelLiveLocation = document.getElementById("levelLive").innerHTML = "is work"
+let totalLivesLocation  = document.getElementById("totalLives").innerHTML = "is work"
+
