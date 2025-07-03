1ccfb9debc50c73db43e688e46df6d4ff36e174b
Jawad <jawad.hassan.office@gmail.com>
2025-07-03 05:27:40 +0300

    add some variable and location, add firstfuncton (only the name)

--- script.js
+++ script.js
@@ -1,4 +1,25 @@
-let currentLevel = document.getElementById("currentLevel").innerHTML = "is work"
-let levelLiveLocation = document.getElementById("levelLive").innerHTML = "is work"
-let totalLivesLocation  = document.getElementById("totalLives").innerHTML = "is work"
+let containerClassLocation = document.getElementById("container")
 
+let statsLocatoin = document.getElementById("stats")
+let currentLevelLocation = document.getElementById("currentLevel")
+let levelLiveLocation = document.getElementById("levelLive") 
+let totalLivesLocation  = document.getElementById("totalLives") 
+
+
+let currentLevel = 1
+let levelLive = 3
+let totalLives = 3
+
+let dimension = 3
+let squerNumber = 3
+
+let tureGuesses = []
+let tureGuessesIndex =[]
+
+let falseGuesses = []
+let falseGuessesIndex =[]
+
+const createSquares = () =>{
+
+
+}
